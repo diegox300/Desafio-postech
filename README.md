4b386900e7569ee1253835ddc147f50246b6e87c
Diego Gervasio <diegox300@gmail.com>
2024-06-07 17:32:47 -0300

    Desafio

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Desafio-postech
 Desafio do primeiro dia de aula na pos-tech FIAP
+
+Criar um formulario com javaScript, html e css, para enviar uma requisição post
+em json.
+
+# Integrantes
+Diego Gervasio RM: 357154
---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let obj = {
+      names: [],
+      message: document.getElementById('message').value
+    }
+    obj.names.push(document.getElementById('nome1').value);
+    obj.names.push(document.getElementById('nome2').value);
+    obj.names.push(document.getElementById('nome3').value);
+    obj.names.push(document.getElementById('nome4').value);
+    obj.names.push(document.getElementById('nome5').value);
+  
+    console.log(obj);
+  
+    fetch('https://fsdt-contact-acf4ab9867a7.herokuapp.com/contact', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error sending data');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data send:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+  });
